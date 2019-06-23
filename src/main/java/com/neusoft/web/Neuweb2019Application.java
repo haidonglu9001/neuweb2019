934803bf9db7029f2201fdebf157b1515cbabbe0
lhd <haidonglu@126.com>
2019-06-23 19:58:03 +0800

    LHD

---
+++ src/main/java/com/neusoft/web/Neuweb2019Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neuweb2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neuweb2019Application.class, args);
+	}
+
+}
